d443b03702ac220f243fda48f79158b8a69ebe4b
yjorfi1 <yjorfi@hotmail.com>
2025-11-12 09:39:03 +0100

    contactpagina

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,18 @@
+
 const Contact = () => {
     return (  
-        <h1 >hallo</h1>
+        <>
+
+ <form className="form-container"action={"https://formspree.io/f/xanayvok"} method="POST">
+    <p className="form-title">Neem contact op</p>
+    <hr />
+    <p className="form-title2">Ik reageer graag!</p>
+    <input className="form-email" type="email" name='email' placeholder={"E-mail"} required /><br />
+    <input className="form-subject" type="text" name='subject'placeholder={"Onderwerp"} required /><br />
+    <input className="form-message" type="text" name='message'placeholder={"Bericht"} required/><br />
+    <input className="form-button" type="submit" value={"Verzenden"} />
+    </form>       
+        </>
     );
 }
  
