d9676fa3342b0e3d8fc1bd403f56cd2a86de2248
yjorfi1 <yjorfi@hotmail.com>
2025-11-18 11:17:59 +0100

    project verder

--- src/Projects.js
+++ src/Projects.js
@@ -7,6 +7,21 @@ desc: "Ik en een klasgenoot hebben samen een website gebouwd als ons eerst proje
 
 },
 
+{
+    id: 2,
+    image: "./img/spel.png",
+name: "Nyan cat spel",
+desc: "Met javascript had ik een nyan cat styled eindeloos runner spel gemaakt",
+
+},
+{
+    id: 3,
+    image: "./img/crud.png",
+name: "Crud page",
+desc: "Ik moest met php voor een website op verschillende manieren met data werken",
+
+}
+
 ];
  
 export default projects;
