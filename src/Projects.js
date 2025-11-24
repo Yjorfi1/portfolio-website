c1866e96ca812c18cb68926aab91c2230fa36d40
yjorfi1 <yjorfi@hotmail.com>
2025-11-24 12:53:28 +0100

    Project af

--- src/Projects.js
+++ src/Projects.js
@@ -1,41 +1,41 @@
-export const  projectdata = 
-[ {
-    id: 1,
-    image: "../img/spel.png",
+export const projectdata =
+    [{
+        id: 1,
+        image: "../img/spel.png",
         image1: "./img/spel.png",
-    image2: "../img/spel2.png",
-name: "Javascript spel",
-desc: "Ik moest in de 1e jaar een javascript spel maken met alleen html css en javascript",
-desc2: "Ik had een simpel nyan cat gestylde oneindig rennerspel gemaakt waar je obstakels moest ontwijken. ik had hier een 70/75 voor gekregen",
-skills: "Html & Css",
+        image2: "../img/spel2.png",
+        name: "Javascript spel",
+        desc: "Ik moest in de 1e jaar een javascript spel maken met alleen html css en javascript",
+        desc2: "Ik had een simpel nyan cat gestylde oneindig rennerspel gemaakt waar je obstakels moest ontwijken. ik had hier een 70/75 voor gekregen",
+        skills: "Html & Css",
 
-},
+    },
 
-{
-    id: 2,
-    image: "../img/escaperoom.png",
+    {
+        id: 2,
+        image: "../img/escaperoom.png",
         image1: "./img/escaperoom.png",
-    image2: "../img/escaperoom2.png",
-name: "Escape Room",
-desc: "In het 1e jaar moest ik met 2 andere leerlingen een escape room bouwen met html css php en javascript",
-desc2: "Wij besloten om met een onderwater thema te gaan waar je de titanic berooft om dingen te verkopen. hier hadden wij een 55/75 punten gekregen",
-skills: "Html, Css, Php & Javascript",
+        image2: "../img/escaperoom2.png",
+        name: "Escape Room",
+        desc: "In het 1e jaar moest ik met 2 andere leerlingen een escape room bouwen met html css php en javascript",
+        desc2: "Wij besloten om met een onderwater thema te gaan waar je de titanic berooft om dingen te verkopen. hier hadden wij een 55/75 punten gekregen",
+        skills: "Html, Css, Php & Javascript",
 
-},
-{
-    id: 3,
-    image: "../img/crud.png",
+    },
+    {
+        id: 3,
+        image: "../img/crud.png",
         image1: "./img/crud.png",
 
-    image2: "../img/crud2.png",
-name: "Crud page",
-desc: "Ik moest met 3 andere leerlingen voor een bestaande website <br />over brood een crud page maken <br />waar je dingen kan toevoegen, verwijderen en updaten",
-desc2: "Ik moest zorgen dat gebruikers en beheerders konden inloggen <br />en uitloggen en ik moest een overzicht maken van <br />alle producten met de bestelling ervoor.",
+        image2: "../img/crud2.png",
+        name: "Crud page",
+        desc: "Ik moest met 3 andere leerlingen voor een bestaande website <br />over brood een crud page maken <br />waar je dingen kan toevoegen, verwijderen en updaten",
+        desc2: "Ik moest zorgen dat gebruikers en beheerders konden inloggen <br />en uitloggen en ik moest een overzicht maken van <br />alle producten met de bestelling ervoor.",
 
-skills: "Php & Html",
+        skills: "Php & Html",
 
-}
+    }
 
-];
- 
+    ];
+
 export default projectdata;
